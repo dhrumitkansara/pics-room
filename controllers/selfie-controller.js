a106a5dc17b390ffc47f2803ab4afe51b80c2ad6
dhrumitkansara <dhrumit@cactuscreatives.com>
2021-10-04 18:52:32 +0530

    Fetch frame data from DB & sending it to selfie UI

--- controllers/selfie-controller.js
+++ controllers/selfie-controller.js
@@ -1,3 +1,6 @@
+// Import statements
+const framesData = require("../models/frames-model");
+
 const { v4: uuidv4 } = require("uuid");
 
 // Controller functions
@@ -18,7 +21,18 @@ exports.virtual_background = (req, res) => {
 };
 
 exports.selfie = (req, res) => {
-  res.render("selfie");
+  // res.render("selfie");
+  framesData
+    .find((err, data) => {
+      if (err) {
+        console.log("Error fetching frames data: ", err);
+        res.status(500).send(err); // Throwing error
+      } else {
+        console.log("Fetched frames data: ", data);
+        res.render("selfie", { framesData: data }); // Rendering profile view and passing fetched profile data to the view
+      }
+    })
+    .sort({ createdAt: -1 });
 };
 
 // Group selfie functions
